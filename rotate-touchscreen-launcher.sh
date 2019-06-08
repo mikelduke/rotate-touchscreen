ee3ef1353903f54024440d01c1131ac1211670f3
Mikel Duke <mikelduke@gmail.com>
2019-06-08 09:20:44 -0500

    add log on init

--- rotate-touchscreen-launcher.sh
+++ rotate-touchscreen-launcher.sh
@@ -3,6 +3,8 @@
 # https://gist.github.com/mildmojo/48e9025070a2ba40795c
 # needs apt install iio-sensor-proxy expect
 # /usr/bin/monitor-sensor \
+logger "rotate-touchscreen-launcher.sh: Init"
+
 monitor-sensor \
 | unbuffer -p grep -o -e right -e normal -e bottom -e left \
 | unbuffer -p ./rotate-touchscreen-loop.sh
